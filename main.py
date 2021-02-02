791f9574b98baa3d0a6288946c92b9f3530c643b
noisemedic <noise.medic@gmail.com>
2021-02-02 19:45:07 +0000

    change to rising

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ GPIO.add_event_callback(8,countme)
 # Set the input with falling edge detection for geiger counter pulses
 GPIO.setup(8, GPIO.IN)
 # ?remove callback feature
-GPIO.add_event_detect(8, GPIO.FALLING, callback=countme)
+GPIO.add_event_detect(8, GPIO.RISING, callback=countme)
 #GPIO.add_event_detect(8, GPIO.FALLING)
 #GPIO.add_event_callback(8, countme)
 
